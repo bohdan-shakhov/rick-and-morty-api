2ca943a2356b3e173d01293b5c27e931b5791564
bohdan-shakhov <bogdanshahov2000@gmail.com>
2022-02-23 16:08:12 +0200

    optimized imports

--- src/main/java/com/example/rickandmorty/controller/UserController.java
+++ src/main/java/com/example/rickandmorty/controller/UserController.java
@@ -1,33 +1,18 @@
 package com.example.rickandmorty.controller;
 
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.rickandmorty.entity.Role;
 import com.example.rickandmorty.entity.User;
 import com.example.rickandmorty.service.UserService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.net.URI;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
